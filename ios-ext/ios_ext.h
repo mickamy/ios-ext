af91d92fcc3d3891228ec2e8e1123ca71f27d944
mickamy <t.mikami1992@gmail.com>
2019-09-28 03:13:16 +0900

    Initial commit

---
+++ ios-ext/ios_ext.h
@@ -0,0 +1,19 @@
+//
+//  ios_ext.h
+//  ios-ext
+//
+//  Created by Tetsuro Mikami on 2019/09/28.
+//  Copyright © 2019 Tetsuro Mikami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ios_ext.
+FOUNDATION_EXPORT double ios_extVersionNumber;
+
+//! Project version string for ios_ext.
+FOUNDATION_EXPORT const unsigned char ios_extVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ios_ext/PublicHeader.h>
+
+
